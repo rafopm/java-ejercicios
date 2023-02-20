8b7ccd1a67e739a9156ffa83fd3ddca427035bdf
rafopm <rafopm@gmail.com>
2023-02-20 17:53:41 -0500

    finish game adivinador

--- adivina-numero/src/adivina/Main.java
+++ adivina-numero/src/adivina/Main.java
@@ -4,9 +4,39 @@ import java.util.Scanner;
 
 public class Main {
 
+	static Nivel[] nivel = new Nivel[] { 
+			new Nivel(0, "", 0), 
+			new Nivel(1, "Muy fácil: ", 10),
+			new Nivel(2, "Fácil", 8), 
+			new Nivel(3, "Medio", 6),
+			new Nivel(4, "Difícil", 5),
+			new Nivel(5, "Muy difícil", 4) };
+	
 	public static void main(String[] args) {
-
-		adivinador(6);
+		while (true) {
+			System.out.println("---------------------------------------");
+			System.out.println("=========  ADIVINA EL NÚMERO  =========");
+			System.out.println("---------------------------------------");
+			for(int i = 1; i < nivel.length; i++) {
+				System.out.println(i+" -> "+ nivel[i].nombreNivel);
+			}
+			System.out.println(nivel.length+" -> SALIR \n");
+			
+			try {
+				Scanner leer = new Scanner(System.in);
+				System.out.print("Escribe la opción: ");
+				int opcion = leer.nextInt();
+				if(opcion == nivel.length) {
+					System.out.println("");
+					System.out.println("HASTA PRONTO...");
+					break;
+				} else if(opcion > 0 && opcion < nivel.length) {
+					adivinador(nivel[opcion].vidas);
+				} 
+			} catch (Exception e) {
+				System.out.println("No es una opción válida");
+			}
+		}
 	}
 
 	static void adivinador(int vidas) {
@@ -16,7 +46,7 @@ public class Main {
 
 		externo:
 		while (aleatorio != numero) {
-			System.out.println("Ingrese un número entre el 1 y el 100: ");
+			System.out.print("Ingrese un número entre el 1 y el 100: ");
 			numero = leer.nextInt();
 			if (aleatorio < numero) {
 				System.out.println("El número es menor");
@@ -28,7 +58,8 @@ public class Main {
 
 			if (vidas == 0) {
 				System.out.printf("Te quedan %d vidas \n", vidas);
-				System.out.println("PERDISTE");
+				System.out.println("GAME OVER");
+				System.out.println("");
 				break externo;
 			} else {
 				System.out.printf("Te quedan %d vidas \n", vidas);
@@ -36,7 +67,10 @@ public class Main {
 
 		}
 		if(aleatorio == numero) {
+			System.out.println("*********************************");
 			System.out.println("VICTORIA: " + aleatorio + " es el número correcto");
+			System.out.println("*********************************");
+			System.out.println("");
 		}
 		
 	}
---
+++ adivina-numero/src/adivina/Nivel.java
@@ -0,0 +1,13 @@
+package adivina;
+
+public class Nivel {
+		public int id;
+		public String nombreNivel;
+		public int vidas;
+
+		public Nivel(int id, String nombreNivel, int vidas) {
+			this.id = id;
+			this.nombreNivel = nombreNivel;
+			this.vidas = vidas;
+		}
+}
